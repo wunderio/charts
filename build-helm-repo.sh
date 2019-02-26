d201db91d67219f8d0be38900f5101e8da06646f
Jānis Bebrītis <janis.bebritis@wunder.io>
2019-02-26 13:25:40 +0200

    Silta-cluster chart

--- build-helm-repo.sh
+++ build-helm-repo.sh
@@ -5,4 +5,7 @@ helm package drupal --destination docs
 
 helm package simple --destination docs
 
+helm dependency build silta-cluster
+helm package silta-cluster --destination docs
+
 helm repo index docs --url https://wunderio.github.io/charts/
