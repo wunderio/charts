0cc54c0ba22b9e7264bf8e7a9e0cde7e3e73506e
Jānis Bebrītis <janis.bebritis@wunder.io>
2020-03-23 15:21:54 +0200

    Drupal chart: https fix and shell github token to secret

--- drupal/files/settings.silta.php
+++ drupal/files/settings.silta.php
@@ -57,11 +57,15 @@ $settings['trusted_host_patterns'][] = '^.*$';
 $config['system.logging']['error_level'] = getenv('ERROR_LEVEL');
 
 /**
+ * Enable reverse proxy
+ */
+$settings['reverse_proxy'] = TRUE;
+$settings['reverse_proxy_addresses'] = [];
+
+/**
  * Override varnish config when varnish environment variables are defined.
  */
 if (getenv('VARNISH_ADMIN_HOST')) {
-  $settings['reverse_proxy'] = TRUE;
-
   $config['varnish.settings']['varnish_version'] = 4;
   $config['varnish.settings']['varnish_control_terminal'] = getenv('VARNISH_ADMIN_HOST') . ':' . getenv('VARNISH_ADMIN_PORT');
   $config['varnish.settings']['varnish_control_key'] = trim(getenv('VARNISH_CONTROL_KEY'));
